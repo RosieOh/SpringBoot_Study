fb83aceadbbd91805008cf4f374a801ca83c6180
RosieOh <20172207@gm.hannam.ac.kr>
2023-11-08 10:25:22 +0900

    🌈feat : thymeleaf를 활용해서 Controller의 내용을 불러오는 연습🌈

--- pro05/src/main/java/com/shop/controller/ExamController.java
+++ pro05/src/main/java/com/shop/controller/ExamController.java
@@ -6,8 +6,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/ex")
@@ -25,6 +26,26 @@ public class ExamController {
 
     @GetMapping("/sample2")
     public String getSample2(Model model) {
+        // 부가 설명 추가하자
+        // Name1 ~ Name10
+        List<String> strList = IntStream.range(1, 10).mapToObj(i -> "Name" + 1).collect(Collectors.toList());
+        model.addAttribute("List", strList);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("k", "KIM");
+        map.put("L", "LEE");
+        map.put("P", "PARK");
+        map.put("S", "SHIN");
+        model.addAttribute("map", map);
+
+        Set<String> set = new HashSet<>();
+        set.add("OH");
+        set.add("HAN");
+        set.add("SEO");
+        set.add("LEE");
+        set.add("HWANG");
+        set.add("HAN");
+        model.addAttribute("set", set);
         return "exam/sample2";
     }
 
--- pro05/src/main/java/com/shop/controller/RootController.java
+++ pro05/src/main/java/com/shop/controller/RootController.java
@@ -16,4 +16,47 @@ public class RootController {
     public String hello(Model model) {
         return "hello";
     }
+
+    @GetMapping("/hello2")
+    public String  hello2(Model model) {
+        Human human = new Human();
+        model.addAttribute("human", human);
+        return "hello2";
+    }
+
+    @GetMapping("/hello3")
+    public String  hello3(Model model) {
+        Human human = new Human();
+        model.addAttribute("human", human);
+        return "hello3";
+    }
+}
+
+class Human {
+    private String name;
+    private int age;
+
+    public Human() {
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
